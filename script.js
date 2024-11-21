60c3f9115b125ab05df190bc8dd797c482fdad31
Emily Alava <emilyalava2004@gmail.com>
2024-11-21 10:57:18 -0500

    editar

--- script.js
+++ script.js
@@ -18,13 +18,27 @@ function mostrar(){
     document.getElementById('btn1');
     var mostrar = document.getElementById('mostrar');
     mostrar.innerHTML='';
-    list.forEach((per)=>{
+    list.forEach((per, cop)=>{
         mostrar.innerHTML += 
         `<tr> 
               <td>${per.nombre}</td>
               <td>${per.telefono}</td>
               <td>${per.correo}</td>
               <td>${per.etiqueta}</td>
+              <td><div class="btn-group" role="group" aria-label="Basic mixed styles example">
+              <button onclick="editar(${cop})" type="button" clas="btn btn-warning">Editar</button>
+              </div>
+              </td>
          </tr> `    
     })
+}
+
+function editar(cop){
+    list.splice(cop,1,{
+        nombre:document.getElementById('nombre').value,
+        telefono:document.getElementById('telefono').value,
+        correo:document.getElementById('correo').value,
+        etiqueta:document.getElementById('etiqueta').value
+    });
+    mostrar();
 }
