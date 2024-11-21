2550a2abb24cc49d2a77792d235d050445ccc11b
Emily Alava <emilyalava2004@gmail.com>
2024-11-21 10:51:14 -0500

    mostrar

--- script.js
+++ script.js
@@ -12,4 +12,19 @@ function agg(){
         document.getElementById('correo').value,
         document.getElementById('etiqueta').value,
     ));
+    mostrar();
+}
+function mostrar(){
+    document.getElementById('btn1');
+    var mostrar = document.getElementById('mostrar');
+    mostrar.innerHTML='';
+    list.forEach((per)=>{
+        mostrar.innerHTML += 
+        `<tr> 
+              <td>${per.nombre}</td>
+              <td>${per.telefono}</td>
+              <td>${per.correo}</td>
+              <td>${per.etiqueta}</td>
+         </tr> `    
+    })
 }
